446bc13d517232ec8f415ff0bcfebbc1d5cffea2
bahieu123123 <ba.hieu0207@gmail.com>
2017-10-02 20:42:49 +0300

    3 lessons

--- test/lesson2/task1/Tests.kt
+++ test/lesson2/task1/Tests.kt
@@ -46,7 +46,7 @@ class Tests {
 
     @Test
     @Tag("Easy")
-    fun rookOrBishopThreatens() {
+        fun rookOrBishopThreatens() {
         assertEquals(0, rookOrBishopThreatens(4, 5, 5, 7, 8, 8))
         assertEquals(1, rookOrBishopThreatens(2, 8, 6, 8, 1, 6))
         assertEquals(2, rookOrBishopThreatens(5, 4, 3, 7, 1, 8))
