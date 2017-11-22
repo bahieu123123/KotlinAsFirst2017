c4147bc5698867e9cea957f6cc70092ba1d7695f
bahieu123123 <ba.hieu0207@gmail.com>
2017-11-22 23:47:01 +0300

    1 2 3 4 5

--- test/lesson5/task1/Tests.kt
+++ test/lesson5/task1/Tests.kt
@@ -99,7 +99,7 @@ class Tests {
     @Test
     @Tag("Hard")
     fun mostExpensive() {
-        assertEquals("", mostExpensive(""))
+        assertEquals("", mostExpensive("~ 0"))
         assertEquals("Курица", mostExpensive("Хлеб 39.9; Молоко 62.5; Курица 184.0; Конфеты 89.9"))
         assertEquals("Вино", mostExpensive("Вино 255.0"))
     }
